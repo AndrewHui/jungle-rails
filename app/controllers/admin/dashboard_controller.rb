030fc871680bed2569d663537463b76e82771e41
Andrew <andrew.j.hui@gmail.com>
2019-10-17 20:01:23 +0000

    updating the admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,5 +5,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASS']
 
   def show
+    @productCount = Product.count
+    @categoryCount = Category.count
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+ 
   create_table "line_items", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
