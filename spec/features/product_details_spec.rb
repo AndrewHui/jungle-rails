b3b2aae4d81e970f32d89b3a73cef119c4bfe3a7
Andrew <andrew.j.hui@gmail.com>
2019-10-22 20:51:09 +0000

    updating test for products

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
+  
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+  
+      1.times do |n|
+        @category.products.create!(
+          name:  Faker::Hipster.sentence(3),
+          description: Faker::Hipster.paragraph(4),
+          image: open_asset('apparel1.jpg'),
+          quantity: 10,
+          price: 64.99
+        )
+      end
+    end
+
+    scenario "They see all products" do
+      # ACT
+      visit root_path
+      click_link ("Details »")
+      
+
+      # expect(page).to have_css 'article.product', count: 10
+  
+  
+      # DEBUG
+      sleep 5
+      save_screenshot
+  
+      # VERIFY
+      # expect(page).to have_css 'article.product'
+    end
+
+end
